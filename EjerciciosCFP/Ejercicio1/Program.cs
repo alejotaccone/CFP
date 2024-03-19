798727ccf4a32ce52907b8c2543a4a737db4a291
Alejo Taccone <alejotaccone11@gmail.com>
2024-03-18 21:18:33 -0300

    Mi primer ejercicio

---
+++ EjerciciosCFP/Ejercicio1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CARACTERES = String
+            //NUMEROS REALES = FLOAT(INPRECISO) - DOBLE(PRESISO) - DECIMAL(EXACTO)
+            //NUMEROS ENTEROS = INT - INT8 - INT16 - INT32  
+            //LOGICO = BOOL
+
+            string nombre;
+            string apellido;
+            string edadString;
+            int edadInt;
+
+            Console.WriteLine("Ingrese su nombre: ");
+
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su apellido: ");
+
+            apellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad: ");
+
+            edadString = Console.ReadLine();
+
+            edadInt = int.Parse(edadString);
+            
+
+            Console.WriteLine("Bienvenido " + nombre + " " + apellido + ", Su edad es: " + edadInt);
+
+            Console.WriteLine("Bienvenido {0} {1}, Su edad es: {2}",nombre,apellido,edadInt);
+
+            Console.WriteLine($"Bienvenido {nombre} {apellido}, Su edad es: {edadInt}");
+
+        }
+    }
+}
